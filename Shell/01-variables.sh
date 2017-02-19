c6f0be50aa7b05efae232ca68df508d97140c47b
Brenton Horne <brentonhorne77@gmail.com>
2017-02-20 01:39:03 +1000

    Update 01-variables.sh

--- Shell/01-variables.sh
+++ Shell/01-variables.sh
@@ -1,2 +1,2 @@
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin:/usr/X11R6/bin:/usr/games
-export PKG_PATH=http://mirror.aarnet.edu.au/pub/OpenBSD/$(uname -r)/packages/$(uname -p)/
+export PKG_PATH=ftp://ftp.netbsd.org/pub/pkgsrc/packages/NetBSD/amd64/7.0_current/All/
